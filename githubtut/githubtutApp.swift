0fc3004391da25eb42e0b6483a31fbb98f778ba8
cheryljustin <cherylkj@gmail.com>
2024-07-12 11:37:00 -0700

    Initial Commit

---
+++ githubtut/githubtutApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubtutApp.swift
+//  githubtut
+//
+//  Created by Justin Jose on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct githubtutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
